2dbdd82b0ca0a34624132ef1edbb1148f6a6b63d
yuyang <yuyang@daishujiankang.com>
2022-05-19 18:17:11 +0800

    feat: modify jest config

--- src/jest.config.js
+++ src/jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
+  transform: {
+    "^.+\\.(t|j)sx?$": "ts-jest"
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)$",
+  moduleFileExtensions: ["ts"]
+  // preset: "ts-jest",
+  // testEnvironment: "node",
   // preset: "ts-jest",
   // transform: { "^.+\\.ts?$": "ts-jest" },
   // moduleFileExtensions: ["ts"],
@@ -8,7 +13,7 @@ module.exports = {
   // coverageDirectory: "coverage",
   // moduleFileExtensions: ['ts'],
   // testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
-  moduleDirectories: ['__test__'],
+  // moduleDirectories: ['__test__'],
   // preset: 'ts-jest/presets/js-with-ts', // or other ESM presets
   // // preset: 'ts-jest/presets/default-esm', // or other ESM presets
   // globals: {

fd1fb78c43a2e1ac252dfe8382c075db818e0906
yuyang <yuyang@daishujiankang.com>
2022-05-20 17:43:14 +0800

    build

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -13,20 +13,39 @@ function getLocation() {
     return 12345;
 }
 
-function getDay() {
-    return new Date();
-}
-function getYear() {
-    return new Date().getFullYear();
+function formatDate(fmt, date) {
+    const o = {
+        'y+': date.getFullYear(),
+        'M+': date.getMonth() + 1,
+        'd+': date.getDate(),
+        'h+': date.getHours(),
+        'm+': date.getMinutes(),
+        's+': date.getSeconds(),
+        'q+': Math.floor((date.getMonth() + 3) / 3),
+        S: date.getMilliseconds(), // 毫秒
+    };
+    // 年份格式
+    if (/(y+)/.test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substring(4 - RegExp.$1.length));
+    }
+    for (const k in o) {
+        if (new RegExp('(' + k + ')').test(fmt)) {
+            // fmt = fmt.replace(RegExp.$1, o[k])
+            // 根据要求补0
+            const val = o[k];
+            fmt = fmt.replace(RegExp.$1, RegExp.$1.length === 1 ? val : ('00' + val).substring(('' + val).length));
+        }
+    }
+    return fmt;
 }
 
-function validateIdCard(x) {
-    return true;
+function validateIdCard(value) {
+    const reg = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/;
+    return reg.test(value);
 }
 
 exports.add = add;
-exports.getDay = getDay;
+exports.formatDate = formatDate;
 exports.getLocation = getLocation;
-exports.getYear = getYear;
 exports.minus = minus;
 exports.validateIdCard = validateIdCard;
--- dist/index.esm.js
+++ dist/index.esm.js
@@ -9,15 +9,35 @@ function getLocation() {
     return 12345;
 }
 
-function getDay() {
-    return new Date();
-}
-function getYear() {
-    return new Date().getFullYear();
+function formatDate(fmt, date) {
+    const o = {
+        'y+': date.getFullYear(),
+        'M+': date.getMonth() + 1,
+        'd+': date.getDate(),
+        'h+': date.getHours(),
+        'm+': date.getMinutes(),
+        's+': date.getSeconds(),
+        'q+': Math.floor((date.getMonth() + 3) / 3),
+        S: date.getMilliseconds(), // 毫秒
+    };
+    // 年份格式
+    if (/(y+)/.test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substring(4 - RegExp.$1.length));
+    }
+    for (const k in o) {
+        if (new RegExp('(' + k + ')').test(fmt)) {
+            // fmt = fmt.replace(RegExp.$1, o[k])
+            // 根据要求补0
+            const val = o[k];
+            fmt = fmt.replace(RegExp.$1, RegExp.$1.length === 1 ? val : ('00' + val).substring(('' + val).length));
+        }
+    }
+    return fmt;
 }
 
-function validateIdCard(x) {
-    return true;
+function validateIdCard(value) {
+    const reg = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/;
+    return reg.test(value);
 }
 
-export { add, getDay, getLocation, getYear, minus, validateIdCard };
+export { add, formatDate, getLocation, minus, validateIdCard };
